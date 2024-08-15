402e16160a35a831b96a50e5f8a5c726d470d063
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-08-15 12:04:21 -0300

    Atualizar reserva entidade

--- src/main/java/br/com/facisa/entities/Reserva.java
+++ src/main/java/br/com/facisa/entities/Reserva.java
@@ -1,7 +1,9 @@
 package br.com.facisa.entities;
 
 import javax.persistence.*;
+import java.security.interfaces.RSAKey;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_reservas")
@@ -22,4 +24,63 @@ public class Reserva {
     @OneToOne
     @JoinColumn(name = "quarto_id")
     private Quarto quarto;
+
+    public Reserva(){
+    }
+
+    public Reserva(LocalDate checkin, LocalDate checkout, Pessoa pessoa, Quarto quarto) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.pessoa = pessoa;
+        this.quarto = quarto;
+    }
+
+    public LocalDate getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(LocalDate checkin) {
+        this.checkin = checkin;
+    }
+
+    public LocalDate getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(LocalDate checkout) {
+        this.checkout = checkout;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reserva reserva = (Reserva) o;
+        return Objects.equals(id, reserva.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
