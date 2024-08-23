28330a657361b4cdff9a12bcea9ea4a6a6d3e851
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-08-23 12:17:43 -0300

    Cria Controller para Funcionario e Hóspede

---
+++ src/main/java/br/com/facisa/resources/FuncionarioController.java
@@ -0,0 +1,108 @@
+package br.com.facisa.resources;
+
+import java.math.BigDecimal;
+
+import javax.swing.JOptionPane;
+
+import br.com.facisa.entities.Funcionario;
+import br.com.facisa.services.FuncionarioService;
+
+public class FuncionarioController {
+	
+	FuncionarioService funcionarioService;
+	
+	public FuncionarioController() {
+		funcionarioService = new FuncionarioService();
+	}
+	
+	public void  FuncionarioController(){
+		String [] opcoes = {"Cadastrar","Editar","Excluir"};
+		
+		Object selecionado = JOptionPane.showInputDialog(null, 
+				"Selecione", "Gerenciamento de funcionário", JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+		
+		String opcaoSelecionada = selecionado.toString();
+		
+		switch (opcaoSelecionada) {
+		
+		case "Cadastrar" : {
+			
+			try {
+				String nome = JOptionPane.showInputDialog("Insira o nome do funcionário");
+				String cpf = JOptionPane.showInputDialog("Insira o cpf do funcionário");
+				String cargo = JOptionPane.showInputDialog("Insira o cargo do funcionário");
+				String salarioString  = JOptionPane.showInputDialog("Insira o salário do funcionário");
+				
+				BigDecimal salario = new BigDecimal(salarioString);
+				
+				String turno = verificarTurno();
+				
+				Funcionario funcionario = new Funcionario(cpf,nome,cargo,salario,turno);
+				
+				String message = funcionarioService.cadastrarFuncionario(funcionario);
+				
+				JOptionPane.showMessageDialog(null,message);
+				
+				
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		case "Editar" : {
+			
+			try {
+				String identificador = JOptionPane.showInputDialog("Digite o id do funcionário :");
+				Long id = Long.parseLong(identificador);
+				String nome = JOptionPane.showInputDialog("Insira o novo nome do funcionário");
+				String cargo = JOptionPane.showInputDialog("Insira o novo cargo do funcionário");
+				String salarioString  = JOptionPane.showInputDialog("Insira o novo salário do funcionário");
+				
+				BigDecimal salario = new BigDecimal(salarioString);
+				
+				String turno = verificarTurno();
+				
+				Funcionario funcionario = new Funcionario(nome,cargo,salario,turno);
+				
+				String message = funcionarioService.editarFuncionario(id, funcionario);
+				
+				JOptionPane.showMessageDialog(null,message);
+			}
+			catch (RuntimeException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+		case "Excluir" : {
+			try {
+				String identificador = JOptionPane.showInputDialog("Digite o id do funcionário :");
+				Long id = Long.parseLong(identificador);
+				
+				String message = funcionarioService.excluirFuncionario(id);
+				
+				JOptionPane.showMessageDialog(null,message);
+			}
+			catch (RuntimeException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		default:
+			
+		}
+	}
+	
+	private String verificarTurno() {
+		
+		String [] turnos = {"Manhã","Tarde","Noite"};
+		Object turnoEscolha = JOptionPane.showInputDialog(null, 
+				"Selecione", "Gerenciamento de funcionário", JOptionPane.INFORMATION_MESSAGE, null, turnos, turnos[0]);
+		
+		return  turnoEscolha.toString();
+		
+	}
+	
+	
+
+}
+
---
+++ src/main/java/br/com/facisa/resources/HospedeController.java
@@ -0,0 +1,111 @@
+package br.com.facisa.resources;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.JOptionPane;
+
+import br.com.facisa.entities.Endereco;
+import br.com.facisa.entities.Hospede;
+import br.com.facisa.services.HospedeService;
+
+public class HospedeController {
+	
+	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	HospedeService hospedeService;
+	
+	public HospedeController() {
+		this.hospedeService = new HospedeService();
+	}
+
+	public void HospedeController() {
+
+		String[] opcoes = { "Cadastrar", "Editar", "Excluir" };
+
+		Object selecionado = JOptionPane.showInputDialog(null, "Selecione", "Gerenciamento de hóspedes",
+				JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+
+		String opcaoSelecionada = selecionado.toString();
+		
+		
+		switch (opcaoSelecionada) {
+		case "Cadastrar" : {
+			
+			try {
+				
+				String nome = JOptionPane.showInputDialog("Insira o nome do hóspede");
+				String cpf = JOptionPane.showInputDialog("Insira o cpf do hóspede");
+				String date = JOptionPane.showInputDialog("Digite a data de nascimento do hóspede no formato dd/MM/yyyy ");
+				String numero  = JOptionPane.showInputDialog("Insira o número do hóspede");
+				String pais = JOptionPane.showInputDialog("País","Insira o país do hóspede");
+				String cidade = JOptionPane.showInputDialog("Cidade","Insira a cidade  do hóspede");
+				
+				LocalDate nascimento = LocalDate.parse(date,formatter);
+				
+				Endereco endereco = new Endereco(pais,cidade);
+				
+				Hospede hospede = new Hospede(cpf,nome,endereco,nascimento,numero);
+				
+				String message = hospedeService.cadastrarHospede(hospede);
+				
+				JOptionPane.showMessageDialog(null,message);
+				
+				
+			}
+			catch (RuntimeException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
+			}
+			
+			
+		}
+		case "Editar" : {
+			
+			try {
+				
+				String identificador = JOptionPane.showInputDialog("Digite o id do hóspede :");
+				Long id = Long.parseLong(identificador);
+				String nome = JOptionPane.showInputDialog("Insira o novo nome do hóspede");
+				String date = JOptionPane.showInputDialog("Digite a data de nascimento do hóspede no formato dd/MM/yyyy ");
+				String numero  = JOptionPane.showInputDialog("Insira o novo número do hóspede");
+				String pais = JOptionPane.showInputDialog("País","Insira o país do hóspede");
+				String cidade = JOptionPane.showInputDialog("Cidade","Insira a cidade  do hóspede");
+				
+				LocalDate nascimento = LocalDate.parse(date,formatter);
+				
+				Endereco endereco = new Endereco(pais,cidade);
+				
+				Hospede hospede = new Hospede(nome, nascimento, endereco, numero);
+				
+				String message = hospedeService.editarHospede(id,hospede);
+				
+				JOptionPane.showMessageDialog(null,message);
+				
+			}
+			catch (RuntimeException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+		case "Excluir" : {
+			
+			try {
+				String identificador = JOptionPane.showInputDialog("Digite o id do hóspede :");
+				Long id = Long.parseLong(identificador);
+				
+				String message = hospedeService.excluirHospede(id);
+				
+				JOptionPane.showMessageDialog(null,message);
+			}
+			catch (RuntimeException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		default:
+		
+		}
+		
+
+	}
+
+}
